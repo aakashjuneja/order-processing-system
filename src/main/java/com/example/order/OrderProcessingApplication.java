d373c25b94657845ec2d914649adcb418fd2b9c2
Aakash Juneja <33748707+aakashjuneja@users.noreply.github.com>
2026-03-20 12:46:26 +0530

    Add comment in main method of OrderProcessingApplication
    Added a comment in the main method.

--- src/main/java/com/example/order/OrderProcessingApplication.java
+++ src/main/java/com/example/order/OrderProcessingApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderProcessingApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderProcessingApplication.class, args);
+        //this comment
     }
 }
+kmoommoijo
